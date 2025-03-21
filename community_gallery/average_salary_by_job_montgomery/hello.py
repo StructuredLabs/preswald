582dee3fcec71e9080ab3c01297d8b0e3f48b95a
therealrinku <rinkunited2032@gmail.com>
2025-03-21 10:43:42 +0545

    fix: update sql queries

--- community_gallery/average_salary_by_job_montgomery/hello.py
+++ community_gallery/average_salary_by_job_montgomery/hello.py
@@ -11,9 +11,11 @@ df = get_df("sample_csv")
 sidebar(defaultopen=False)
 
 text("# Top 10 Job position with highest average base salary.", size=0.1)
-top_10_df_sql = (
-    """SELECT * FROM sample_csv ORDER BY "Numeric Base Salary" DESC LIMIT 10"""
-)
+top_10_df_sql = """
+    select *
+    from sample_csv
+    order by cast(replace(replace("Average Of Base Salary", '$', ''), ',', '') as integer) desc limit 10;
+"""
 top_10_df = query(top_10_df_sql, "sample_csv")
 # Create a scatter plot
 top_10_fig = px.scatter(
@@ -34,9 +36,11 @@ top_10_fig.update_layout(template="plotly_white")
 plotly(top_10_fig)
 
 text("# Bottom 10 Job position with lowest average base salary.", size=0.1)
-bottom_10_df_sql = (
-    """SELECT * FROM sample_csv ORDER BY "Numeric Base Salary" LIMIT 10"""
-)
+bottom_10_df_sql = """
+    select *
+    from sample_csv
+    order by cast(replace(replace("Average Of Base Salary", '$', ''), ',', '') as integer) limit 10;
+"""
 bottom_10_df = query(bottom_10_df_sql, "sample_csv")
 # Create a scatter plot
 bottom_10_fig = px.scatter(
@@ -60,6 +64,13 @@ plotly(bottom_10_fig)
 threshold = slider(
     "Average Base Salary Greater than", min_val=30000, max_val=250000, default=30000
 )
-sorted_data_sql = """SELECT * FROM sample_csv ORDER BY "Numeric Base Salary" """
+sorted_data_sql = """
+    select *, cast(replace(replace("Average of Base Salary", '$', ''), ',', '') as integer) as "Numeric Average Of Base Salary"
+    from sample_csv
+    order by cast(replace(replace("Average Of Base Salary", '$', ''), ',', '') as integer);
+"""
 sorted_data = query(sorted_data_sql, "sample_csv")
-table(sorted_data[sorted_data["Numeric Base Salary"] > threshold], title="Full Data")
+table(
+    sorted_data[sorted_data["Numeric Average Of Base Salary"] > threshold],
+    title="Full Data",
+)
