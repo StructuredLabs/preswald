8b888e4b559d1e34787705ec4e558eddbf088e97
therealrinku <rinkunited2032@gmail.com>
2025-04-04 10:46:58 +0545

    fix: update text sizes

--- community_gallery/average_salary_by_job_montgomery/hello.py
+++ community_gallery/average_salary_by_job_montgomery/hello.py
@@ -2,22 +2,20 @@ from preswald import text, plotly, connect, get_df, table, query, slider, sideba
 import pandas as pd
 import plotly.express as px
 
-text("# Average Salary by Job Classification on Montgomery County of Maryland.")
-
-# Load the CSV
-connect()  # load in all sources, which by default is the sample_csv
+connect()
 df = get_df("sample_csv")
+
+text("## Average Salary by Job Classification on Montgomery County of Maryland.")
 
 sidebar(defaultopen=False)
 
-text("# Top 10 Job position with highest average base salary.", size=0.1)
+text("### Top 10 Job position with highest average base salary.", size=0.1)
 top_10_df_sql = """
     select *
     from sample_csv
     order by cast(replace(replace("Average Of Base Salary", '$', ''), ',', '') as integer) desc limit 10;
 """
 top_10_df = query(top_10_df_sql, "sample_csv")
-# Create a scatter plot
 top_10_fig = px.scatter(
     top_10_df,
     x="Position Title",
@@ -26,23 +24,19 @@ top_10_fig = px.scatter(
     title="Position vs. Average Base Salary",
     labels={"quantity": "Quantity", "value": "Value"},
 )
-# Add labels for each point
 top_10_fig.update_traces(
     textposition="top center", marker=dict(size=12, color="lightblue")
 )
-# Style the plot
 top_10_fig.update_layout(template="plotly_white")
-# Show the plot
 plotly(top_10_fig)
 
-text("# Bottom 10 Job position with lowest average base salary.", size=0.1)
+text("### Bottom 10 Job position with lowest average base salary.", size=0.1)
 bottom_10_df_sql = """
     select *
     from sample_csv
     order by cast(replace(replace("Average Of Base Salary", '$', ''), ',', '') as integer) limit 10;
 """
 bottom_10_df = query(bottom_10_df_sql, "sample_csv")
-# Create a scatter plot
 bottom_10_fig = px.scatter(
     bottom_10_df,
     x="Position Title",
@@ -51,16 +45,13 @@ bottom_10_fig = px.scatter(
     title="Position vs. Average Base Salary",
     labels={"quantity": "Quantity", "value": "Value"},
 )
-# Add labels for each point
 bottom_10_fig.update_traces(
     textposition="top center", marker=dict(size=12, color="lightblue")
 )
-# Style the plot
 bottom_10_fig.update_layout(template="plotly_white")
-# Show the plot
 plotly(bottom_10_fig)
 
-# Show the data
+text("### Full data")
 threshold = slider(
     "Average Base Salary Greater than", min_val=30000, max_val=250000, default=30000
 )
@@ -70,7 +61,4 @@ sorted_data_sql = """
     order by cast(replace(replace("Average Of Base Salary", '$', ''), ',', '') as integer);
 """
 sorted_data = query(sorted_data_sql, "sample_csv")
-table(
-    sorted_data[sorted_data["Numeric Average Of Base Salary"] > threshold],
-    title="Full Data",
-)
+table(sorted_data[sorted_data["Numeric Average Of Base Salary"] > threshold])
